e1cf1b11c456d1ecf5faa77051c176c0dcefbf4d
Santino Amodeo <santinoamodeoph@gmail.com>
2025-09-09 09:36:56 -0300

    variables negadas

--- simulador_arduino/simulador_arduino.ino
+++ simulador_arduino/simulador_arduino.ino
@@ -15,6 +15,16 @@ int ARD_OUT_FC_TAMBOR_ARRIBA = 4;
 int ARD_OUT_FC_TAMBOR_ABAJO = 5;
 int PWM_PRESION = 6;  // Salida PWM de presión
 
+//Negar salidas PLC
+int EscotillaValvula = !digitalRead(PLC_OUT_ESCOTILLA_E_VALVULA);
+int Inflar = !digitalRead(PLC_OUT_INFLAR);
+int Descargar = !digitalRead(PLC_OUT_DESCARGAR);
+int Alivio = !digitalRead(PLC_OUT_VALVULA_ALIVIO);
+int GiroHorario = !digitalRead(PLC_OUT_GIRO_HOR);
+int GiroAntihorario = !digitalRead(PLC_OUT_GIRO_ANTIHOR);
+
+
+
 // Variables internas
 float presion = 0.0;
 bool escotillaCerrada = true;
@@ -50,14 +60,14 @@ void loop() {
   // --- ESCOTILLA --- (Abrir y cerrar compuerta + fines de carrera de salida)
 
   //Escotilla abierta
-  if(digitalRead(PLC_OUT_ESCOTILLA_E_VALVULA) == HIGH) {
+  if(digitalRead(EscotillaValvula) == HIGH) {
     delay(200);
     digitalWrite(ARD_OUT_FC_ESCOTILLA_ABIERTA, HIGH);
     digitalWrite(ARD_OUT_FC_ESCOTILLA_CERRADA, LOW);
   }
 
   //Escotilla cerrado
-  if(digitalRead(PLC_OUT_ESCOTILLA_E_VALVULA) == LOW){
+  if(digitalRead(EscotillaValvula) == LOW){
     delay(200);
     digitalWrite(ARD_OUT_FC_ESCOTILLA_ABIERTA, LOW);
     digitalWrite(ARD_OUT_FC_ESCOTILLA_CERRADA, HIGH);
@@ -68,19 +78,19 @@ void loop() {
   // --- CASOS CURVAS --- 
 
   // INFLAR
-  if((digitalRead(PLC_OUT_VALVULA_ALIVIO) == HIGH)&& (presion<1.1) && digitalRead(PLC_OUT_INFLAR) == LOW){  //venteo de negativo a 1.2
+  if((digitalRead(Alivio) == HIGH)&& (presion<1.1) && digitalRead(Inflar) == LOW){  //venteo de negativo a 1.2
         presion = presion + (objetivo_aliviar - presion) * 0.1;
         delay(200);
       }
 
-  if((digitalRead(PLC_OUT_VALVULA_ALIVIO) == HIGH) && (digitalRead(PLC_OUT_INFLAR) == HIGH)){
+  if((digitalRead(Alivio) == HIGH) && (digitalRead(Inflar) == HIGH)){
     if((1 < presion) &&  (presion < 1.3)){
         presion = presion + (1.3 - presion) * 0.1;  //nivelacion a 1.3
         delay(50);
     }
   }
 
-  if((digitalRead(PLC_OUT_INFLAR) == HIGH) && (digitalRead(PLC_OUT_VALVULA_ALIVIO) == LOW)){
+  if((digitalRead(Inflar) == HIGH) && (digitalRead(Alivio) == LOW)){
     if((presion>1.1) && (presion<1.4))
         presion = presion + (objetivo_inflar - presion) * 0.1;  //subir hasta 2.2
         delay(200);
@@ -89,21 +99,21 @@ void loop() {
 
 
   // DESINFLAR
-  if((digitalRead(PLC_OUT_VALVULA_ALIVIO) == HIGH) && (digitalRead(PLC_OUT_DESCARGAR) == LOW)){
+  if((digitalRead(Alivio) == HIGH) && (digitalRead(Descargar) == LOW)){
     if(presion > 1){
       presion = presion - (presion - objetivo_aliviar) * 0.1;  //bajar a 1.2
       delay(200);
     }
   }
 
-  if((digitalRead(PLC_OUT_VALVULA_ALIVIO) == HIGH) && (digitalRead(PLC_OUT_DESCARGAR) == HIGH)){
+  if((digitalRead(Alivio) == HIGH) && (digitalRead(Descargar) == HIGH)){
     if((1 < presion) &&  (presion < 1.3)){
       presion = presion + (1 - presion) * 0.1;  //nivelacion a 1
       delay(50);
     }
   }
   
-  if((digitalRead(PLC_OUT_DESCARGAR) == HIGH) && (digitalRead(PLC_OUT_VALVULA_ALIVIO) == LOW)){
+  if((digitalRead(Descargar) == HIGH) && (digitalRead(Alivio) == LOW)){
     if((presion>0.9) && (presion<1.3))
       presion = presion + (objetivo_descargar - presion) * 0.1;  //bajar hasta -0.15
   }
@@ -150,23 +160,23 @@ void loop() {
 
 
   // --- Giros del tambor (1 vuelta cada 3 segundos) ---
-  if ((digitalRead(PLC_OUT_GIRO_HOR) == HIGH ) and (cont=!359)){
+  if ((digitalRead(GiroHorario) == HIGH ) and (cont=!359)){
   cont++;
   delay(100);    
 
   }
   
- if ((digitalRead(PLC_OUT_GIRO_ANTIHOR) == HIGH) and (cont!=0)){
+ if ((digitalRead(GiroAntihorario) == HIGH) and (cont!=0)){
   cont--;
   delay(100);
 
   }
-  if ((digitalRead(PLC_OUT_GIRO_HOR) == HIGH ) and (cont==359)) {
+  if ((digitalRead(GiroHorario) == HIGH ) and (cont==359)) {
 
     cont=0;
     delay(100);    
   }
-  if ((digitalRead(PLC_OUT_GIRO_ANTIHOR) == HIGH) and (cont==0)){
+  if ((digitalRead(GiroAntihorario) == HIGH) and (cont==0)){
     cont=359;
     delay(100);
   }
